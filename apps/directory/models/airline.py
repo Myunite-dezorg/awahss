a328ed99cf978a7ddc0173798e0bd50cbde34e3e
Andrew_KHZz <andrewkharzin@gmail.com>
2023-03-22 20:14:01 +0000

    Img hnd

--- apps/directory/models/airline.py
+++ apps/directory/models/airline.py
@@ -8,20 +8,23 @@ from django.utils.html import mark_safe
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 def link_airline_image(instance, **kwargs):
     if instance.codeIataAirline:
         filename = f"{instance.ncodeIataAirlineame}.png"
-        filepath = os.path.join(settings.MEDIA_ROOT, 'airlines/square', filename)
+        filepath = os.path.join(settings.MEDIA_ROOT,
+                                'airlines/square', filename)
         if os.path.exists(filepath):
             instance.arl_logo = os.path.join('airlines/square', filename)
             instance.save()
 
 
 def airline_banner_directory_path(instance, filename):
-        codeIataAirline = slugify(instance.codeIataAirline)
-        codeIcaoAirline = slugify(instance.codeIcaoAirline)
-        _, extension = os.path.splitext(filename)
-        return f"airlines/banners/{codeIataAirline}/{codeIcaoAirline}{extension}"
+    codeIataAirline = slugify(instance.codeIataAirline)
+    codeIcaoAirline = slugify(instance.codeIcaoAirline)
+    _, extension = os.path.splitext(filename)
+    return f"airlines/banners/{codeIataAirline}/{codeIcaoAirline}{extension}"
+
 
 class Airline(models.Model):
 
@@ -31,22 +34,24 @@ class Airline(models.Model):
     ageFleet = models.IntegerField(null=True)
     founding = models.IntegerField(null=True)
     sizeAirline = models.IntegerField(null=True)
-    statusAirline = models.CharField(_("StatusAirline"), max_length=155, default="", blank=True)
+    statusAirline = models.CharField(
+        _("StatusAirline"), max_length=155, default="", blank=True)
     iataPrefixAccounting = models.IntegerField(null=True)
     callsign = models.CharField(_("Callsign"), max_length=155, default="")
-    codeHub  = models.CharField(_("CodeHUB"), max_length=5, default="")
-    codeIso2Country = models.CharField(_("CodeIso2Country"), max_length=4, default="", blank=True)
+    codeHub = models.CharField(_("CodeHUB"), max_length=5, default="")
+    codeIso2Country = models.CharField(
+        _("CodeIso2Country"), max_length=4, default="", blank=True)
     codeIataAirline = models.CharField(_("Iata"), max_length=3, default="")
     codeIcaoAirline = models.CharField(_("Icao"), max_length=4, default="")
     nameAirline = models.CharField(
         _("Description eng"), max_length=85, default="", blank=True)
-    nameCountry = models.CharField(_("Country"), max_length=85, default="", null=True, blank=True)
+    nameCountry = models.CharField(
+        _("Country"), max_length=85, default="", null=True, blank=True)
     type = models.CharField(_("Type"), max_length=155, default="", blank=True)
     arl_logo = models.ImageField(
         upload_to=link_airline_image)
     banner_img = models.ImageField(
         upload_to=airline_banner_directory_path, blank=True, null=True)
-    
 
     # def link_image(self):
     #     filenames = os.listdir('media/airlines/square')
@@ -60,7 +65,6 @@ class Airline(models.Model):
         if self.arl_logo:
             return mark_safe('<img src="{}" width="50" />'.format(self.arl_logo.url))
         return ""
-
 
     def __str__(self):
         return f"{self.codeIataAirline.upper()}"
